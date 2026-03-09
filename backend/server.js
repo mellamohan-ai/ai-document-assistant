cbc525089194c4e290d16fbd818f547b30877b03
Mella Mohan Kumar <mellamohan.kumar331@gmail.com>
2026-03-10 01:16:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,10 @@ app.use(express.json());
 // serve frontend folder
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-// homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-// ask endpoint
-app.post("/ask", (req, res) => {
-  const question = req.body.question;
-
-  console.log("User Question:", question);
-
-  res.json({
-    answer: "AI response will appear here"
-  });
-});
-
 app.listen(PORT, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log(`Server running on port ${PORT}`);
 });
